0bc410806ddf19e0ae0d287c6a1319a72bb4e9f9
Øystein Haugnes Aune <oysteinhaugnesaune@ysteins-MacBook-Pro.local>
2019-12-14 19:58:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
+
+app.get('/', (req, res) => {
+  const date = new Date();
+  const today = date.getDay();
+
+  if (today === 6 || today === 0) {
+    res.send("Yay it's weekend!!! :D :D")
+  } else {
+    res.send("Boo! It's a working day!")
+  }
+
+  res.sendFile(`${__dirname}/public/index.html`)
+})
