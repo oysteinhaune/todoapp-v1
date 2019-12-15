eb50c5036a2194af2a98dd9ee1a44debdf7613f4
oystein <oysteinha@gmail.com>
2019-12-15 18:35:30 +0100

    app.js update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
+app.use("view engine", "ejs")
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
